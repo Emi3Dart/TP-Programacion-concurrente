4857fd23b5977fef0ddce2686424e739d116f27e
Alejo Fookes <alejofookes@gmail.com>
2026-06-01 12:37:18 -0300

    main | subida de codigo fuente parcial

---
+++ aging.h
@@ -0,0 +1,8 @@
+#ifndef AGING_H_INCLUDED
+#define AGING_H_INCLUDED
+
+#include "pcv.h"
+
+bool agingActivado(const Job& tarea);
+
+#endif // AGING_H_INCLUDED
---
+++ pcv.h
@@ -0,0 +1,32 @@
+#ifndef PCV_H
+#define PCV_H
+
+#include <string>
+#include <chrono>
+
+struct Job {
+    int id; // Identificador unico del Job
+    int prioridad; // 1 = premium / 0 = free
+    std::chrono::steady_clock::time_point llegada;  // Medicion de tiempo de espera encolado (anti-starvation)
+};
+
+void apiGateway(); // Oroductor
+void workerNode(); // Consumidor
+
+void logEvento(const Job& tarea,
+               const std::string& evento);
+
+// ------------------------------
+// CONFIGURACIÓN GENERAL DEL SISTEMA
+// ------------------------------
+
+const int tareasGateway = 10; // Cantidad de tareas que genera cada gateway. Ej = 3 productores (gateway) * 10 tareas = 30 Jobs.
+// (PRUEBA DE VACUIDAD) tareasGateway = 0.
+// (PRUEBA DE CARGA MASIVA) tareasGateway = 500 (Teniendo 3 productores) = 1500 jobs. ESCENARIO B Y C.
+// (PRUEBA DE CARGA MASIVA) tareasGateway = 1500 (Teniendo 1 productor) = 1500 jobs. ESCENARIO A.
+
+extern int NUM_WORKERS;
+extern int NUM_GATEWAYS;
+extern int tareasFinalizadas;
+
+#endif // PCV_H_INCLUDED
